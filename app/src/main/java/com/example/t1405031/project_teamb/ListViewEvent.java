a70e01b4b2e95ff66f254a8aa0cd3397394ba09b
kumadani <kumadanikazuya@yahoo.co.jp>
2016-12-22 18:31:11 +0900

    ブランチ確認その2

--- app/src/main/java/com/example/t1405031/project_teamb/ListViewEvent.java
+++ app/src/main/java/com/example/t1405031/project_teamb/ListViewEvent.java
@@ -12,5 +12,6 @@ public class ListViewEvent extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view);
 
+        //リストビュー作成
     }
 }
