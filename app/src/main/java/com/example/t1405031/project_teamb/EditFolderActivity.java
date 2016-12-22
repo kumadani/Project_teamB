99181a3754b9dcc491c1639f22f984533e5e8b2e
kumadani <kumadanikazuya@yahoo.co.jp>
2016-12-22 18:04:01 +0900

    ブランチ確認

--- app/src/main/java/com/example/t1405031/project_teamb/EditFolderActivity.java
+++ app/src/main/java/com/example/t1405031/project_teamb/EditFolderActivity.java
@@ -9,5 +9,7 @@ public class EditFolderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_folder);
+
+        //checkout確認
     }
 }
